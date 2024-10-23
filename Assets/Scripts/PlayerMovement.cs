a81e3f4b5bc95b32fd5e31cc886992ec5f56e595
John Khachian <jaksawesome10@gmail.com>
2024-10-23 14:25:11 -0400

    added player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,21 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
+    protected Rigidbody2D rigidBody;
     enum Direction { Left, Right };
     private Direction direction;
+    public int moveSpeed;
 
     // Start is called before the first frame update
     void Start() {
         direction = Direction.Right;
+        rigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update() {
         if(Input.GetKey(KeyCode.RightArrow)) {
             direction = Direction.Right;
+            rigidBody.velocity = new Vector2(moveSpeed, 0);
         }
         if(Input.GetKey(KeyCode.LeftArrow)) {
             direction = Direction.Left;
+            rigidBody.velocity = new Vector2(-1 * moveSpeed, 0);
         }
     }
 }
